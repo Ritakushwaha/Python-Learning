e2bf2cd87ada4b2bd095f73108ca328359fa0683
Ritakushwaha <ritakushwaha731997@gmail.com>
2021-12-29 16:59:34 +0530

    Practice

---
+++ add_2_numbers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 18:21:11 2021
+
+@author: rita
+"""
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    a=int(input())
+    b=int(input())
+
+    print(a+b)
+
+main()
+
+
