89a5d077c5a513265c27ffc5778b1f4f7138fdf1
Ritakushwaha <ritakushwaha731997@gmail.com>
2023-10-24 19:13:53 +0530

    keywords in python

---
+++ Codes/get_python_keywords_list.py
@@ -0,0 +1,11 @@
+import keyword
+
+print("Total number of keywords in python")
+print(len(keyword.kwlist))
+print("List of keywords in python")
+print(keyword.kwlist)
+
+print("Total number of soft keywords in python")
+print(len(keyword.softkwlist))
+print("List of soft keywords in python")
+print(keyword.softkwlist)
