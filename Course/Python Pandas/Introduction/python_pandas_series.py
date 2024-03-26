fba6f7585d73dcd60940cb98a4b99e2c7d43fbe8
Ritakushwaha <ritakushwaha731997@gmail.com>
2024-03-27 01:54:21 +0530

    Python pandas series

--- Course/Python Pandas/Introduction/python_pandas_series.py
+++ Course/Python Pandas/Introduction/python_pandas_series.py
@@ -7,11 +7,44 @@ Data: This can be any data type, such as integers, floats, strings, Python objec
 Index: This is a sequence of labels that uniquely identifies each element in the Series. The index can be integers, strings, or any other data type that can be used as labels.
 '''
 
-# Create a Python Pandas Series
 import pandas as pd
 
-# Creating a Series from a list
+## Creating a Series from a list
 data = [10, 20, 30, 40, 50]
 series = pd.Series(data)
-
+print("Series:")
 print(series)
+
+## Accessing Element from Series with Position
+# The position starts at zero for the first element in the series.
+value_at_position_zero = series [0]
+print("\nValue at position zero:")
+print(value_at_position_zero)
+
+## Accessing Elements using Label based Indexing
+series = pd.Series(data, index=['A','B','C','D','E'])
+value_at_label = series['C']
+print("\nValue at label 'C':")
+print(value_at_label)
+
+## Slicing a Series
+slice_of_series = series[3:len(series)]
+print("\nSlice of series:")
+print(slice_of_series)
+
+## Stepping through a Series
+step_through_series = series[::2] # This will return every second element starting from index 0. # It is equivalent to series[0:len(series):2].
+print("\nStepped through series:")
+print(step_through_series)
+
+## Indexing a Series using indexing operator []
+print("\nIndexing using indexing operator []:")
+print(series[3:6])
+
+## Indexing a Series using .loc[ ]
+print("\nIndexing using .loc[]:")
+print(series.loc['C']) #provide label
+
+## Indexing a Series using .iloc[ ]
+print("\nIndexing using .iloc[]:")
+print(series[2:4])
